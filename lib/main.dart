45d224e4da5379def964f48cff20c12a82526ba6
Mohamed Ali <57603152+Mohamed-Yassien@users.noreply.github.com>
2020-05-24 02:28:10 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:work_space/screens/bmi_screens/input_page.dart';
+import 'package:work_space/screens/splash_screen.dart';
 
-import 'src/pages/welcome_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -21,6 +21,6 @@ class MyApp extends StatelessWidget {
             textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
               bodyText1: GoogleFonts.montserrat(textStyle: textTheme.bodyText1),
             )),
-        home: WelcomePage());
+        home: SplashScreen());
   }
 }
